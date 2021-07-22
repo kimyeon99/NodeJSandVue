f80b665b22a38c94878ccce5056042d691587f6d
예승재 <79109492+kimyeon99@users.noreply.github.com>
2021-07-22 20:48:22 +0900

    Add files via upload
    07-22 실습

--- test.js
+++ test.js
@@ -1,57 +1,24 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-            message:'select a user',
-            users:[{ name:'User' }, {name:"Admin"}],
-    },   
+Vue.component('add', {
+    template: '<button @click="add">{{count}}</button>',
+    data(){
+        return {count : 0}
+    },
     methods:{
-        sayHi(name){
-            this.message='hi, ' + name
+        add(){
+            this.count +=1
+            this.$emit('event-add', 2);
         }
     }
 });
 
-let app2= new Vue({
-    el:'#app2',
+let app = new Vue({
+    el:'#app',
     data:{
-        comment:'',
-        link:'https://www.naver.com'
-    }
-});
-
-let app3= new Vue({
-    el:'#app3',
-    data:{
-        comment:''
-    }
-});
-
-let app4= new Vue({
-    el:'#app4',
-    data:{
-        value=false,
-        values:[]
-    }
-});
-
-let app5= new Vue({
-    el:'#app5',
-    data:{
-        age:20
-    }
-})
-
-let app6= new Vue({
-    el:'#app6',
-    data:{
-        value:'hello',
-        value2:'hello'
-    }
-})
-
-let app7 = new Vue({
-    el:'#app7',
-    data:{
-        Amessage:''
+        totalCount:0
+    },
+    methods:{
+        updateTotal(count){
+            this.totalCount += count
+        }
     }
 })
