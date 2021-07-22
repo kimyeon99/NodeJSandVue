7e9dd8470a2f391dff9e1c82f478805ccd5151a8
예승재 <79109492+kimyeon99@users.noreply.github.com>
2021-07-22 20:48:44 +0900

    Add files via upload
    07-22 실습

---
+++ TodoListItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>
+    {{ todo.text }}
+    <button @click="$emit('remove', todo.id)">X</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Obejct,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
