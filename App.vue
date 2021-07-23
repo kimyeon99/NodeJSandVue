196063a29a6b0bc3e40e8592070b954d0aa06461
예승재 <79109492+kimyeon99@users.noreply.github.com>
2021-07-23 16:51:38 +0900

    Add files via upload
    my-cafe

--- App.vue
+++ App.vue
@@ -1,19 +1,19 @@
-<template>
+<template lang="">
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/menus">Menus</router-link></li>
+      <li><router-link to="/events">Events</router-link></li>
+      <li><router-link to="/reviews">Reviews</router-link></li>
+    </ul>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
