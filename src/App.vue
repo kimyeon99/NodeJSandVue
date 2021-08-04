4bc7ded71739b8507fc74a31814b256593951c57
예승재 <79109492+kimyeon99@users.noreply.github.com>
2021-08-04 20:46:58 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+    <v-btn to="/" text>
+      <v-icon>mdi-home</v-icon>
+      <span class="mr-2">Home</span>
+    </v-btn>
+    
+    <v-btn to="/menus" text><span class="mr-2">Menus</span></v-btn>
+    <v-btn to="/events" text><span class="mr-2">Events</span></v-btn>
+    <v-btn to="/reviews" text><span class="mr-2">reviews</span></v-btn>
+
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Menus.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id = "app">
+        <h1>Menus</h1>
+        <h1>{{ menu_name }}</h1>
+        <h1>{{searched_menu}}</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'Menus',
+        computed:{
+            menu_name(){
+                const id = this.$route.params.menu_id
+                if(id == '1') return '아이스 아메리카노'
+                else if(id == '2') return '새싹보리'
+                else return ''
+            },
+            searched_menu(){
+                const id = this.$route.query.menu_id
+                if(id=='1') return '아이스 아메리카노2'
+                else if(id=='2') return '새싹보리2'
+                else return ''
+            }
+        }
+    };
+    
+</script>
